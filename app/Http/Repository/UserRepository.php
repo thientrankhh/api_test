87b6436ecb982174947f3f7cae3e06420346ed7a
thientrankhh <thientrankhh@gmail.com>
2020-05-15 15:02:21 +0700

    [++] Check mail @dienhoa.com

--- app/Http/Repository/UserRepository.php
+++ app/Http/Repository/UserRepository.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Repository;
 
+use Illuminate\Support\Facades\Http;
+use App\Http\Controllers\Controller;
 use App\Model\Overtime;
 use App\User;
+use Illuminate\Http\Response;
+use Illuminate\Support\Carbon;
 
 class UserRepository
 {
     public static function getOvertime($id)
     {
         return Overtime::find($id);
+    }
+
+    public static function checkEmailCompany(string $email)
+    {
+        if (strpos($email, '@dienhoa1080.com') == false) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public static function findUserByEmail(string $email)
@@ -24,5 +37,16 @@ class UserRepository
     public static function getAllNames()
     {
         return User::select('id', 'name')->get();
+    }
+
+    public static function abc($authenticated_user)
+    {
+        $scopes = json_decode($authenticated_user->role->scopes);
+        $tokenResult = $authenticated_user->createToken('User Access Token', $scopes);
+        $token = $tokenResult->token;
+        $token->expires_at = Carbon::now()->addYears(1);
+        $token->save();
+
+        $access_token = $tokenResult->accessToken;
     }
 }
